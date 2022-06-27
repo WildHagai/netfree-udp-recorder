c2cfbaa2a0906efd029f3cd5521ca1a7d0a96923
Hagai Wild <wildhagai@gmail.com>
2022-06-28 00:31:36 +0300

    Added -N flag to tshark, Not sure how useful it is

--- udp_recorder.bat
+++ udp_recorder.bat
@@ -2,4 +2,4 @@
 set tshark="C:\Program Files\Wireshark\tshark.exe"
 %tshark% -D
 set /p interface="Please choose an interface: "
-%tshark% -i %interface% -f "icmp" -Y "icmp.code == 10" -T fields -e ip.dst_host -e udp.dstport
+%tshark% -N d -N n -i %interface% -f "icmp" -Y "icmp.code == 10" -T fields -e ip.dst_host -e udp.dstport
