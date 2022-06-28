1d406dedf7b0fcd3bb540a099b21fca4e64e3870
Hagai Wild <wildhagai@gmail.com>
2022-06-28 21:05:03 +0300

    Changed batch script according to @ynigun's awesome pull request

--- udp_recorder.bat
+++ udp_recorder.bat
@@ -2,4 +2,4 @@
 set tshark="C:\Program Files\Wireshark\tshark.exe"
 %tshark% -D
 set /p interface="Please choose an interface: "
-%tshark% -N d -N n -i %interface% -f "icmp" -Y "icmp.code == 10" -T fields -e ip.dst_host -e udp.dstport
+%tshark% -N dnN -i %interface% -f "icmp" -Y "icmp.code == 10" -T fields -e ip.dst_host -e udp.dstport -eframe.protocols -E occurrence=l
